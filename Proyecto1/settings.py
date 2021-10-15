df751f3c3ecb16d1722d0f99e1de3238c0b33846
davidatp93 <92590641+davidatp93@users.noreply.github.com>
2021-10-15 17:08:23 -0500

    Update de archivos estaticos

--- Proyecto1/settings.py
+++ Proyecto1/settings.py
@@ -129,11 +129,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
+PROJECT_ROOT = os.path.join(os.path.abspath(__file__))
+
 STATICFILES_DIRS = [
      os.path.join(PROJECT_ROOT, 'static')
 ]
 
-PROJECT_ROOT   =   os.path.join(os.path.abspath(__file__))
 STATIC_ROOT  =   os.path.join(PROJECT_ROOT, 'staticfiles')
 STATIC_URL = '/static/'
 
