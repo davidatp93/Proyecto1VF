ea81e17fc9af7f91861adffde58040aa79120277
davidatp93 <92590641+davidatp93@users.noreply.github.com>
2021-10-15 16:49:23 -0500

    Update settings.py

--- Proyecto1/settings.py
+++ Proyecto1/settings.py
@@ -141,5 +141,5 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-MEDIA_URL ='/http://localhost:8000/static/img/'
+MEDIA_URL ='/static/img/'
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
